94839d590eef1dd0861315076232f7b0e631e8b3
screenm0nkey <digital@nicklowman.co.uk>
2015-01-23 14:28:12 +0000

    Stripped out the setting of view path and templateUrl as we're using yeoman to generate the paths for us.

--- angular-component-factory.js
+++ angular-component-factory.js
@@ -1,67 +1,63 @@
 (function (angular) {
-  'use strict';
+    'use strict';
 
-  var originalModule = angular.module;
-  angular.module = function () {
-    var module = originalModule.apply(this, arguments);
+    var componentFactoryProvider = function () {
 
-    module.provider('componentFactory', componentFactoryProvider);
+        var componentFactory = function (componentName, overrides) {
+            var componentSnakeName = componentName
+                .replace(/(?:[A-Z]+)/g, function (match) { //camelCase -> snake-case
+                    return '-' + match.toLowerCase();
+                })
+                .replace(/^-/, ''); // CamelCase -> -snake-case -> snake-case
 
-    module.component = function (name, factoryFn) {
-      module.config(['$compileProvider', function ($compileProvider) {
-        $compileProvider.directive(name + 'Component', ['$injector', 'componentFactory', function ($injector, componentFactory) {
-          return componentFactory(name, $injector.invoke(factoryFn || angular.noop) || {});
-        }]);
-      }]);
-    };
-    return module;
-  };
+            var _default = {
+                replace: true,
+                scope: {},
+                restrict: 'E',
+                controllerAs: 'vm',
+                componentSnakeName: componentSnakeName
+            };
 
-  var componentFactoryProvider = function () {
-
-    var componentBaseViewPath = 'views/components/';
-
-    //Default component view path factory
-    var componentViewPathFactory = function (componentSnakeName, componentName) {
-        return componentBaseViewPath + componentSnakeName + '/' + componentSnakeName + '.html';
-      },
-
-      componentFactory = function (componentName, overrides) {
-
-        var componentSnakeName = componentName
-          .replace(/(?:[A-Z]+)/g, function (match) { //camelCase -> snake-case
-            return "-" + match.toLowerCase();
-          })
-          .replace(/^-/, ''); // CamelCase -> -snake-case -> snake-case
-
-        var _default = {
-          templateUrl: componentViewPathFactory(componentSnakeName, componentName),
-          replace: true,
-          scope: {},
-          restrict: 'E',
-          componentSnakeName: componentSnakeName
+            return angular.extend(_default, overrides);
         };
 
-        if (overrides && overrides.template) delete _default.templateUrl;
-
-        return angular.extend(_default, overrides);
-      };
-
-    this.setViewPath = function (args) {
-      if (typeof args == 'string') {
-        componentBaseViewPath = args;
-      }
-      else if (typeof args == 'function') {
-        componentViewPathFactory = args;
-      }
+        this.$get = function () {
+            return componentFactory;
+        }
     };
 
-    this.$get = function () {
-      return componentFactory;
-    }
-  };
+    var decorateModule = function (module) {
+        //We need to handle components that might be registered before angular has finished loading
+        var queue = [];
+        //This only pushes constructors to a queue, and when angular is ready it registers the directives
+        module.component = function (name, constructor) {
+            queue.push({name: name, constructor: constructor});
+        };
 
-  //Expose provider as a angular module
-  angular.module('kennethlynne.componentFactory', []).provider('componentFactory', componentFactoryProvider);
+        module.config(['$compileProvider', function ($compileProvider) {
+            module.component = function (name, constructor) {
+                //Register decorated directives
+                $compileProvider.directive((name + 'Component'), ['$injector', 'componentFactory', function ($injector, componentFactory) {
+                    return componentFactory(name, $injector.invoke(constructor || angular.noop) || {});
+                }]);
+                return module; //To allow chaining
+            };
+
+            //Registered queued components
+            angular.forEach(queue, function (component) {
+                module.component(component.name, component.constructor);
+            });
+        }]);
+
+        module.provider('componentFactory', componentFactoryProvider);
+    };
+
+    //Expose provider as a angular module
+    angular.module('kennethlynne.componentFactory', []).provider('componentFactory', componentFactoryProvider);
+
+    //Expose decorator
+    angular.componentFactory = {
+        moduleDecorator: decorateModule
+    }
 
 }(angular));
